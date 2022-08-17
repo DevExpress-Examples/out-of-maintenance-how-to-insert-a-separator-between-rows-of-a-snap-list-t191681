068864a6b846c38c9bf8b79270bfbade58cabdd4
DevExpressExampleBot <fakeEmail>
2022-08-17 13:22:26 +0400

    Source auto update [skip ci]

--- VB/SnapSeparatorExample/Form1.vb
+++ VB/SnapSeparatorExample/Form1.vb
@@ -1,35 +1,35 @@
-﻿#Region "#Reference"
+'#Region "#Reference"
 Imports System
 Imports System.Data
 Imports System.Windows.Forms
 Imports DevExpress.Snap.Core.API
 Imports DevExpress.XtraRichEdit.API.Native
-' ...
-#End Region ' #Reference
 
+' ...
+'#End Region  ' #Reference
 Namespace SnapSeparatorExample
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        #Region "#Code"
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+'#Region "#Code"
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' Create a dataset and bind the Snap control to data.
-            Dim ds As New DataSet()
-            Dim dt As New DataTable("Customers")
+            Dim ds As DataSet = New DataSet()
+            Dim dt As DataTable = New DataTable("Customers")
             dt.Columns.Add("Id", GetType(Integer))
             dt.Columns.Add("Name", GetType(String))
             dt.Rows.Add(1, "Steven Buchanan")
             dt.Rows.Add(2, "Anne Dodsworth")
             dt.Rows.Add(3, "Janet Levering")
             ds.Tables.Add(dt)
-            Me.snapControl1.DataSources.Add("DS", ds)
-
+            snapControl1.DataSources.Add("DS", ds)
             ' Create a Snap list and populate it with the data.
-            Dim snList As SnapList = Me.snapControl1.Document.CreateSnList(Me.snapControl1.Document.Range.Start, "List1")
+            Dim snList As SnapList = snapControl1.Document.CreateSnList(snapControl1.Document.Range.Start, "List1")
             snList.BeginUpdate()
             snList.DataSourceName = "DS"
             snList.DataMember = "Customers"
@@ -38,14 +38,12 @@ Namespace SnapSeparatorExample
             Dim listRowCells As TableCellCollection = listRowTable.FirstRow.Cells
             listRow.CreateSnText(listRowCells(0).ContentRange.End, "Id")
             listRow.CreateSnText(listRowCells(1).ContentRange.End, "Name")
-
             ' Define a list separator and specify its format.
-            snList.Separator.AppendText(New String(ControlChars.FormFeed, 1))
-
+            snList.Separator.AppendText(New String(Microsoft.VisualBasic.Strings.ChrW(12), 1))
             ' Finalize the Snap list creation.
             snList.EndUpdate()
             snList.Field.Update()
         End Sub
-        #End Region ' #Code
+'#End Region  ' #Code
     End Class
 End Namespace
